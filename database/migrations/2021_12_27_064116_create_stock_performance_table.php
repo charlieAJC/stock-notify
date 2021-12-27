1f4bb955b0ac9717594af7e4418f38860d382c83
charliehsu <stu610042002@yahoo.com.tw>
2021-12-28 01:59:42 +0800

    修改 "股票近期表現" migration 結構 & config 時區

--- database/migrations/2021_12_27_064116_create_stock_performance_table.php
+++ database/migrations/2021_12_27_064116_create_stock_performance_table.php
@@ -18,9 +18,25 @@ class CreateStockPerformanceTable extends Migration
             $table->id();
             $table->string('stock_symbol', 50)->index()->comment('股票代號');
             $table->date('crawled_at')->comment('爬蟲資料建立時間');
+            $table->decimal('three_days')->comment('三日');
+            $table->decimal('one_week')->comment('一週');
+            $table->decimal('two_weeks')->comment('兩週');
+            $table->decimal('this_month')->comment('本月');
+            $table->decimal('one_month')->comment('一個月');
+            $table->decimal('one_quarter')->comment('一季');
+            $table->decimal('half_year')->comment('半年');
+            $table->decimal('this_year')->comment('今年');
+            $table->decimal('one_year')->comment('一年');
+            $table->decimal('highest_this_year')->comment('自今年高點');
+            $table->decimal('lowest_this_year')->comment('自今年低點');
+            $table->decimal('two_years')->comment('兩年');
+            $table->decimal('three_years')->comment('三年');
+            $table->decimal('five_years')->comment('五年');
             $table->timestamps();
+
+            $table->unique(['stock_symbol', 'crawled_at']);
         });
-        DB::statement("ALTER TABLE `stock_performance` comment '股票近期表現'");
+        DB::statement("ALTER TABLE `stock_performance` comment '股票近期表現 注意數據已去除百分號'");
     }
 
     /**
